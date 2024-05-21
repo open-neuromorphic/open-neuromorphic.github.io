929156d6f34869722058265a5fa29bc97dbf65c6
Dylan Richard Muir <dylan.muir@synsense.ai>
2024-05-21 16:14:25 +0800

    * Added several Jax and torch CUDA-graph benchmarks for Rockpool * Updated Dockerfile to include some missed dependencies * Added descriptive assertion error message to benchmarking function

--- content/english/blog/spiking-neural-network-framework-benchmarking/docker/run_benchmarks.sh
+++ content/english/blog/spiking-neural-network-framework-benchmarking/docker/run_benchmarks.sh
@@ -6,7 +6,7 @@ echo "running benchmarks with batch size $1"
 
 echo "framework,neurons,forward,backward,memory" >data.csv
 
-for benchmark in "rockpool_torch" "rockpool_exodus" "sinabs" "sinabs_exodus" "norse" "snntorch" "spikingjelly" "spikingjelly_cupy" "lava" "spyx_full" "spyx_half"; do
+for benchmark in "rockpool_jax_cpu" "rockpool_native" "rockpool_torch" "rockpool_exodus" "rockpool_jax_tpu" "rockpool_torch_cuda_graph" "rockpool_jax_gpu" "sinabs" "sinabs_exodus" "norse" "snntorch" "spikingjelly" "spikingjelly_cupy" "lava" "spyx_full" "spyx_half"; do
     python3 ./run_benchmark.py $benchmark $1
 done
 
