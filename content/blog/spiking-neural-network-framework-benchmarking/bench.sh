f81d10a35da8f7e8304c7fec7a3975c6702bc27b
Justin Riddiough <jriddiough@gmail.com>
2026-06-29 22:57:55 -0700

    Move benchmarking library to its own repository

--- content/blog/spiking-neural-network-framework-benchmarking/bench.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker rm snn-bench
-docker run -it --gpus all --name snn-bench snn-bench ./run_benchmarks.sh $1
-docker cp snn-bench:/app/data.csv .
-docker cp snn-bench:/app/fig/ .
-cp ./fig/* .
-rm -fr fig
